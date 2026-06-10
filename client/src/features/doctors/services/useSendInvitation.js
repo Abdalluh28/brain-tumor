6286ba8fae1c4f3c29d631eb7b60a39991edebe4
Abdalluh28 <abdok7496@gmail.com>
2026-06-10 21:14:12 +0300

    doctor management - invite doctors to radiology center

--- client/src/features/doctors/services/useSendInvitation.js
+++ client/src/features/doctors/services/useSendInvitation.js
@@ -1,11 +1,17 @@
 import { sendInvitationApi } from "@/services/invitationApi";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 export function useSendInvitation() {
+
+    const queryClient = useQueryClient();
+
     const { mutate, isPending } = useMutation({
         mutationFn: (data) => sendInvitationApi(data),
         onSuccess: (data) => {
+            queryClient.invalidateQueries({
+                queryKey: ["availableDoctors"],
+            })
             toast.success(`Invitation sent to doctor ${data?.name}`);
         },
         onError: (error) => {
