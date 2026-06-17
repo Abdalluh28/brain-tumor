0c5fb7d9f83511c95af2ec9cdda27812f6bbc996
Abdalluh28 <abdok7496@gmail.com>
2026-06-17 18:07:20 +0300

    search by patient ID placeholder

--- client/src/features/history/SearchBar.jsx
+++ client/src/features/history/SearchBar.jsx
@@ -11,7 +11,7 @@ export default function SearchBar() {
             <Search className="text-gray-400 absolute left-3 top-1/2 transform -translate-y-1/2" />
             <input
                 type="text"
-                placeholder="Search by Patient Name"
+                placeholder="Search by Patient Name or Patient ID"
                 className="w-full pl-10 pr-4 py-3 border border-gray-300 dark:border-gray-700 rounded-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 value={searchValue}
                 onChange={handleSearch} />
